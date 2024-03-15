4fb85186c9cd6895f22e107b83ef1e9038603ee1
haseebhy7 <163605192+haseebhy7@users.noreply.github.com>
2024-03-16 04:23:32 +0500

    Delete js/menuBarAnimation.js

--- js/menuBarAnimation.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-  $(".menubar").on("click", function () {
-    gsap.to("#navigation-content", { duration: 0.6, y: 0, ease: "power2.out" });
-  });
-
-  $(".navigation-close").on("click", function () {
-    gsap.to("#navigation-content", { duration: 0.6, y: "-100%", ease: "power2.out" });
-  });
-});
